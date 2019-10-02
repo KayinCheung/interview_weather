0b83df6f6996ad78c758b535ae65943f6718bd53
KayinCheung <cheungkayin00@gmail.com>
2019-10-02 10:06:14 +0800

    Capitalize first letter in recommend buttons

--- src/RecommendButtons.js
+++ src/RecommendButtons.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class RecommendButtons extends Component {
     render() {
+      let cityInput = this.props.cityInput.charAt(0).toUpperCase() + this.props.cityInput.substring(1);
       console.log(this.props.recommendations)
       return(
         <div className="is-centered has-text-centered">
@@ -10,7 +11,7 @@ class RecommendButtons extends Component {
         {Object.keys(this.props.recommendations).map((country) => (
           <button key={`${country}_button`} className="button" 
             onClick={() => this.props.handleCityClick(country, this.props.recommendations[country])}>
-          {this.props.cityInput}, {country}
+          {cityInput}, {country}
           </button>
         ))}
         </div>
